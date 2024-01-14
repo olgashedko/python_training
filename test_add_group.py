d7f27c7395421f5ef64d5de4a44663df3e81540d
Olga Shedko <olga.shedko@gmail.com>
2024-01-14 18:26:10 +0300

    Class group was added

--- test_add_group.py
+++ test_add_group.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import Group
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
@@ -15,19 +16,19 @@ class UntitledTestCase(unittest.TestCase):
     def return_to_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def geate_new_group(self, wd, group_name, group_header, group_footer):
+    def geate_new_group(self, wd, group):
         # create new group
         wd.find_element_by_name("new").click()
         # fill group parameters
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group_name)
+        wd.find_element_by_name("group_name").send_keys(group.group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group_header)
+        wd.find_element_by_name("group_header").send_keys(group.group_header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group_footer)
+        wd.find_element_by_name("group_footer").send_keys(group.group_footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -51,7 +52,7 @@ class UntitledTestCase(unittest.TestCase):
             self.open_home_page(wd)
             self.login(wd, username="admin", password="secret")
             self.open_group_page(wd)
-            self.geate_new_group(wd, group_name="test", group_header="test", group_footer="test")
+            self.geate_new_group(wd, Group(group_name="test", group_header="test", group_footer="test"))
             self.return_to_group_page(wd)
             self.logout(wd)
     def test_add_empty_group(self):
@@ -59,7 +60,7 @@ class UntitledTestCase(unittest.TestCase):
             self.open_home_page(wd)
             self.login(wd, username="admin", password="secret")
             self.open_group_page(wd)
-            self.geate_new_group(wd, group_name="", group_header="", group_footer="")
+            self.geate_new_group(wd, Group(group_name="", group_header="", group_footer=""))
             self.return_to_group_page(wd)
             self.logout(wd)
     def is_element_present(self, how, what):
