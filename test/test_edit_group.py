5f34c46329778c411fbb4b0cd2b7c42cdc83fa75
Olga Shedko <olga.shedko@gmail.com>
2024-01-26 21:48:25 +0300

    edit_group, modify_group methods are modified

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,4 +4,6 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
+    if app.groupHelp.count() == 0:
+        app.groupHelp.create_new_group(Group(group_name="group1"))
     app.groupHelp.edit_first_group(Group(group_name="test", group_header="test", group_footer="test"))
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,12 +4,18 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    if app.groupHelp.count() == 0:
+        app.groupHelp.create_new_group(Group(group_name="group1"))
     app.groupHelp.modify_first_group(Group(group_name="New name"))
 
 
 def test_modify_group_header(app):
+    if app.groupHelp.count() == 0:
+        app.groupHelp.create_new_group(Group(group_name="group1"))
     app.groupHelp.modify_first_group(Group(group_header="New header"))
 
 
 def test_modify_group_footer(app):
+    if app.groupHelp.count() == 0:
+        app.groupHelp.create_new_group(Group(group_name="group1"))
     app.groupHelp.modify_first_group(Group(group_footer="New footer"))
