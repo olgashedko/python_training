211b9d86b0533340d8dccd0dea40f5be417a6309
Olga Shedko <olga.shedko@gmail.com>
2024-01-26 21:36:33 +0300

    delete_group method is modified

--- test/test_group_delete.py
+++ test/test_group_delete.py
@@ -1,5 +1,8 @@
 import time
+from model.group import Group
 
 
 def test_delete_first_group(app):
+    if app.groupHelp.count() == 0:
+        app.groupHelp.create_new_group(Group(group_name="group1"))
     app.groupHelp.delete_first_group()
