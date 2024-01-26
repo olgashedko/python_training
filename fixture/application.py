f4205ac0b0c55d9c8f6c76428a42c4d74255a542
Olga Shedko <olga.shedko@gmail.com>
2024-01-26 21:02:06 +0300

    ensure_login is added

--- fixture/application.py
+++ fixture/application.py
@@ -12,16 +12,16 @@ class Application:
         self.groupHelp = GroupHelper(self)
         self.contactHelp = ContactHelper(self)
 
-    def is_valid(self):
-        try:
-            self.wd.current_url
-            return True
-        except:
-            return False
-
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
     def destroy(self):
         self.wd.quit()
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
