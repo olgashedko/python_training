2172fc0f5c47ad3d47c484777f74c85bcabc9263
Olga Shedko <olga.shedko@gmail.com>
2024-02-16 21:48:40 +0300

    configuration file is added

--- conftest.py
+++ conftest.py
@@ -1,23 +1,24 @@
+import json
+
 import pytest
 from model.group import Group
 from fixture.application import Application
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--base_url")
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            browser = request.config.getoption("--browser")
-            base_url = request.config.getoption("--base_url")
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    global target
+    browser = request.config.getoption("--browser")
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -33,4 +34,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--base_url", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
